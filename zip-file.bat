669f24f945039594a4d51e3233edfb67ce14ccf6
Roh8am <111047435+Roh8am@users.noreply.github.com>
2022-09-01 16:21:00 +0530

    Update zip-file.bat

--- zip-file.bat
+++ zip-file.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET Source=D:\tasks\Copy-Script\Source\file*
+SET Source=D:\task\copy-script\Source\file*
 
 SET Desination=D:\tasks\pipeline_using_bat\test1.zip
 SET zipUtility="C:\Program Files\7-Zip\7z.exe"
